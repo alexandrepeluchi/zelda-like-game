53ab2fe86abc3af97f2ce97d1a1d3453c219832a
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-07-29 21:58:28 -0300

    Script para rodar a animação dependendo da direção do sprite

--- player/player.gd
+++ player/player.gd
@@ -4,9 +4,18 @@ const SPEED = 70
 
 var movedir = Vector2(0,0)
 
+# A direção que a sprite vai iniciar
+var spritedir = "down"
+
 func _physics_process(delta):
 	controls_loops()
 	movement_loop()
+	spritedir_loop()
+	
+	if movedir != Vector2(0,0):
+		anim_switch("walk")
+	else:
+		anim_switch("idle")
 
 func controls_loops():
 	var UP		= Input.is_action_pressed("ui_up")
@@ -21,5 +30,19 @@ func movement_loop():
 	var motion = movedir.normalized() * SPEED
 	move_and_slide(motion, Vector2(0,0))
 	
-	
+func spritedir_loop():
+	match movedir:
+		Vector2(0,-1):
+			spritedir = "up"
+		Vector2(0,1):
+			spritedir = "down"
+		Vector2(-1,0):
+			spritedir = "left"
+		Vector2(1,0):
+			spritedir = "right"
+			
+func anim_switch(animation):
+	var newAnim = str(animation, spritedir)
+	if $anim.current_animation != newAnim:
+		$anim.play(newAnim)		
 	
