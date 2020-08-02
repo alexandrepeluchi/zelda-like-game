f1f89dca34deaceb0e2449145b593af9530076fc
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-02 11:12:24 -0300

    Player herdando script entity

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,6 @@
-extends KinematicBody2D
+extends entity
 
-const SPEED = 70
-
-var movedir = Vector2(0,0)
-
-# A direção que a sprite vai iniciar
-var spritedir = "down"
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	controls_loops()
 	movement_loop()
 	spritedir_loop()
@@ -34,24 +27,4 @@ func controls_loops():
 	
 	movedir.y = - int(UP) + int(DOWN)	
 	movedir.x = -int(LEFT) + int(RIGHT)
-	
-func movement_loop():
-	var motion = movedir.normalized() * SPEED
-	move_and_slide(motion, Vector2(0,0))
-	
-func spritedir_loop():
-	match movedir:
-		Vector2(0,-1):
-			spritedir = "up"
-		Vector2(0,1):
-			spritedir = "down"
-		Vector2(-1,0):
-			spritedir = "left"
-		Vector2(1,0):
-			spritedir = "right"
-			
-func anim_switch(animation):
-	var newAnim = str(animation, spritedir)
-	if $anim.current_animation != newAnim:
-		$anim.play(newAnim)		
 	
