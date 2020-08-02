0ab9f5dbefd09222b33afe7685d2827c101039b7
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-02 11:51:56 -0300

    Movimentação aleatória do inimigo 'stalfos'

--- enemies/stalfos.gd
+++ enemies/stalfos.gd
@@ -1,4 +1,16 @@
 extends entity
 
+var movetimer_length = 15
+var movetimer = 0
 
-
+func _ready():
+	$anim.play("default")
+	movedir = dir.rand()
+	
+func _physics_process(_delta):
+	movement_loop()	
+	if movetimer > 0:
+		movetimer -= 1
+	if movetimer == 0 || is_on_wall():
+		movedir = dir.rand()
+		movetimer = movetimer_length
