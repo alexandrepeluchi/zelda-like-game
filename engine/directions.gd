d99097a596d77dba3af2f575b473982c86f1d184
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-02 11:36:00 -0300

    Criado script singleton para movimentação

---
+++ engine/directions.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const center = Vector2(0,0)
+const up = Vector2(0,-1)
+const down = Vector2(0,1)
+const left = Vector2(-1,0)
+const right= Vector2(1,0)
+
+func rand():
+	var random = randi() % 4 + 1
+	match random:
+		1: 
+			return up
+		2: 
+			return down
+		3:
+			return left
+		4:
+			return right
+	
