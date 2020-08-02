215d482ec9f3b39f26306d33285abfd03c57f55a
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-02 14:07:41 -0300

    Função hitstun

--- engine/entity.gd
+++ engine/entity.gd
@@ -1,15 +1,20 @@
 class_name entity extends KinematicBody2D
 
+export(String) var TYPE = "ENEMY" 
 export(int) var SPEED = 0
 
-var movedir = dir.center
+var movedir = dir.stopped
+var knockdir = dir.stopped
 
 # A direção que a sprite vai iniciar
 var spritedir = "down"
 
+var hitstun = 0
+var health = 1
+
 func movement_loop():
 	var motion = movedir.normalized() * SPEED
-	move_and_slide(motion, dir.center)
+	move_and_slide(motion, dir.stopped)
 	
 func spritedir_loop():
 	match movedir:
@@ -26,3 +31,13 @@ func anim_switch(animation):
 	var newAnim = str(animation, spritedir)
 	if $anim.current_animation != newAnim:
 		$anim.play(newAnim)		
+		
+func damage_loop():
+	if hitstun > 0:
+		hitstun -= 1
+	for body in $hitbox.get_overlapping_bodies():
+		if hitstun == 0 and body.get("DAMAGE") != null and body.get("TYPE") != TYPE:
+			health -= body.get("DAMAGE")
+			hitstun = 10
+			knockdir = transform.origin - body.transform.origin
+	
