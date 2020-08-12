4deaa8c0f140d32a4781df1672e0351b81e21082
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-11 22:06:53 -0300

    Deleta items que podem ser pegos ao sair da area

--- engine/camera.gd
+++ engine/camera.gd
@@ -3,6 +3,7 @@ extends Camera2D
 func _ready():
 	$area.connect("body_entered", self, "body_entered")
 	$area.connect("body_exited", self, "body_exited")
+	$area.connect("area_exited", self, "area_exited")
 
 func _process(_delta):
 	# Camera only work if it's on the same level of the player scene
@@ -24,3 +25,8 @@ func body_exited(body):
 	if body.get("TYPE") == "ENEMY":
 		body.set_physics_process(false)
 
+func area_exited(area):
+	# Using get because if an area doesn't have the disappears the game crash
+	if area.get("disappears") == true:
+		# Delete pickups when player exit the area
+		area.queue_free()
