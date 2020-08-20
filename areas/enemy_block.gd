08e7b59c4b77fc027ee334a32614731d88f871cd
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-19 21:15:03 -0300

    Comentando os scripts

--- areas/enemy_block.gd
+++ areas/enemy_block.gd
@@ -1,12 +1,18 @@
 extends TileMap
 
+# Load block scene
 var block = preload("res://tiles/block.tscn")
 
 func _ready():
+	# Make every block scene invible
 	self.visible = false
-	
+	# Get the sprite id from the tilemap
 	var block_tile = get_used_cells_by_id(0)
+	# Loop that cycles through all enemy blocks added to the scene
 	for tile in block_tile:
+		# Create and add a instance to a variable
 		var block_instance = block.instance()
+		# Find the correct intance global position based on the cell size
 		block_instance.global_position = map_to_world(tile) + cell_size / 2
+		# Added the enemy block to the scene
 		add_child(block_instance)
--- tiles/enemy_door.gd
+++ tiles/enemy_door.gd
@@ -1,17 +1,23 @@
 extends StaticBody2D
 
+# When the app is ready find the camera and player node
 onready var camera = get_node("../camera")
 onready var player = get_node("../player")
 
 func _ready():
+	# Open the doors when game starts
 	$anim.play("open")
 	
 func _process(delta):
 	if camera.grid_pos == camera.get_grid_pos(global_position):
+		# If there is no enemy on the screen
 		if camera.get_enemies() == 0:
+			# Open the door
 			if $anim.assigned_animation != "open":
 				$anim.play("open")
+			# If player enter the room
 		elif !$area.get_overlapping_bodies().has(player):
+			# Close the door
 			if $anim.assigned_animation != "close":
 				$anim.play("close")
 	else:
